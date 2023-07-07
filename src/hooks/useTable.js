8f658462feafc6eb74a263945807c415781e077e
thais de rodez <thaisderodez@gmail.com>
2023-07-07 14:25:29 +0100

    feat(useTable): table logic

---
+++ src/hooks/useTable.js
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import {
+  getIndexes,
+  getNumberOfPages,
+  getPageRange,
+  getSlicedData,
+  searchThroughObject,
+} from "../utils/utils";
+
+/**
+ * Custom hook to handle table actions
+ * @param {object[]} data Data to displayed in the table
+ * @param {number} page Current page displayed
+ * @param {number} rowsPerPage Number of rows displayed per page
+ * @param {string} searchInput Search input
+ * @param {"asc" | "desc" | "none"} sorting Type of sorting
+ * @param {string} sortingKey Key sorted on
+ * @returns
+ */
+export const useTable = (
+  data,
+  page,
+  rowsPerPage,
+  searchInput,
+  sorting,
+  sortingKey
+) => {
+  const [pageRange, setPageRange] = useState([]);
+  const [paginatedData, setPaginatedData] = useState([]);
+  const [numberOfPages, setNumberOfPages] = useState(
+    getNumberOfPages(data, rowsPerPage)
+  );
+  const [indexes, setIndexes] = useState(getIndexes(page, rowsPerPage));
+  const [totalOfRows, setTotalOfRows] = useState(data.length);
+
+  useEffect(() => {
+    const range = getPageRange(numberOfPages);
+    setPageRange([...range]);
+
+    const currentIndexes = getIndexes(page, rowsPerPage);
+    setIndexes({ ...currentIndexes });
+
+    let result = getSlicedData(data, page, rowsPerPage);
+    let currentNumberOfPages = getNumberOfPages(data, rowsPerPage);
+    let currentTotalOfRows = data.length;
+
+    if (searchInput.length > 0) {
+      const searchResult = data.filter((obj) =>
+        searchThroughObject(obj, searchInput)
+      );
+      currentNumberOfPages = getNumberOfPages(searchResult, rowsPerPage);
+      currentTotalOfRows = searchResult.length;
+      result = getSlicedData(searchResult, page, rowsPerPage);
+    }
+    if (sortingKey.length > 0 && sorting === "asc") {
+      result = result.sort((a, b) =>
+        b[sortingKey]
+          .toString()
+          .localeCompare(a[sortingKey].toString())
+      );
+    } else if (sortingKey.length > 0 && sorting === "desc") {
+      result = result.sort((a, b) =>
+        a[sortingKey]
+          .toString()
+          .localeCompare(b[sortingKey].toString())
+      );
+    }
+    setTotalOfRows(currentTotalOfRows);
+    setNumberOfPages(currentNumberOfPages);
+    setPaginatedData(result);
+  }, [
+    data,
+    rowsPerPage,
+    page,
+    numberOfPages,
+    searchInput,
+    sorting,
+    sortingKey,
+  ]);
+
+  return { paginatedData, pageRange, numberOfPages, indexes, totalOfRows };
+};
---
+++ src/utils/utils.js
@@ -0,0 +1,70 @@
+/**
+ * Get the list of page numbers
+ * @param {number} numberOfPages Number of page
+ * @returns {number[]} List of page numbers
+ */
+export const getPageRange = (numberOfPages) => {
+  const range = [];
+  for (let i = 1; i <= numberOfPages; i++) {
+    range.push(i);
+  }
+  return range;
+};
+
+/**
+ * Get the first and last indexes of data displayed on a page
+ * @param {number} page Current page
+ * @param {number} rowsPerPage Number of rows per page
+ * @returns {{firstIndex: number; lastIndex: number;}} First and last index displayed on a page
+ */
+export const getIndexes = (page, rowsPerPage) => {
+  const lastIndex = page * rowsPerPage;
+  const firstIndex = lastIndex - rowsPerPage;
+
+  return { firstIndex, lastIndex };
+};
+
+/**
+ * Get the data displayed in one page
+ * @param {Object[]} data Array of data displayed in Table
+ * @param {number} page Current page
+ * @param {number} rowsPerPage Number of rows displayed per page
+ * @returns {Object[]} Data displayed on one page
+ */
+export const getSlicedData = (
+  data,
+  page,
+  rowsPerPage
+) => {
+  const { firstIndex, lastIndex } = getIndexes(page, rowsPerPage);
+
+  return data.slice(firstIndex, lastIndex);
+};
+
+/**
+ * Result of a search in one item
+ * @param {object} obj One data item
+ * @param {string} searchInput
+ * @returns {boolean} Whether the search input can be found in the item
+ */
+export const searchThroughObject = (
+  obj,
+  searchInput
+) => {
+  const result = [];
+  for (const [_, value] of Object.entries(obj)) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    result.push(value.toString().search(searchInput));
+  }
+  return result.some((n) => n !== -1);
+};
+
+/**
+ * Get total number of pages
+ * @param {object[]} data Array of data displayed in Table
+ * @param {number} rowsPerPage Number of rows displayed per page
+ * @returns {number} Number of pages for all data
+ */
+export const getNumberOfPages = (data, rowsPerPage) => {
+  return Math.ceil(data.length / rowsPerPage);
+};
