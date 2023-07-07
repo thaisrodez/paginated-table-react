4252f117cbd4cdf705be08e80450ebd0c0e2f3b7
thais de rodez <thaisderodez@gmail.com>
2023-07-07 14:26:07 +0100

    feat(fullTable): full table layout and logic

---
+++ src/lib/Table/index.js
@@ -0,0 +1,6 @@
+export { DisplayedRows } from "./displayedRows";
+export { RowsPerPage } from "./displayEntries";
+export { Pagination } from "./pagination";
+export { SearchEntries } from "./searchEntries";
+export { Table } from "./table";
+export { default } from "./fullTable";
